092535e83258fd445cb8052ffdd610056ef32be3
dk649 <58308212+dk649@users.noreply.github.com>
2024-12-30 10:52:39 +0000

    updates
    added overlay

--- script.js
+++ script.js
@@ -3,16 +3,26 @@ window.onload = function () {
   const menuEl = document.getElementById("open-sidebar-button");
   const navbarEl = document.getElementById("navbar");
   const closeEl = document.getElementById("close-sidebar-button");
+  const overlayEl = document.getElementById("overlay");
+
   function openSideBar() {
     // console.log("open sidebar");
     navbarEl.classList.add("show");
+    menuEl.setAttribute("aria-expanded", "true");
   }
 
   function closeSidBar() {
     navbarEl.classList.remove("show");
+    menuEl.setAttribute("aria-expanded", "false");
   }
 
   menuEl.addEventListener("click", () => openSideBar());
 
   closeEl.addEventListener("click", () => closeSidBar());
+
+  overlayEl.addEventListener("click", () => {
+    // console.log("overlay clicked");
+
+    closeSidBar();
+  });
 };
